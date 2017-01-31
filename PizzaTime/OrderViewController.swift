a847d5ab56bb53afe66c01d18d336e35117d6bb5
dwilliamscode <DWisYourBestChoice@gmail.com>
2017-01-31 16:17:40 -0600

    remove unnecessary code

--- PizzaTime/OrderViewController.swift
+++ PizzaTime/OrderViewController.swift
@@ -97,69 +97,11 @@ class OrderViewController: UITableViewController {
             print ("\(o.toppings) and \(o.numberOfTimesOrdered)")
           }
           
-          
-          
-          //          if let pizzas : [NSDictionary] = jsonResult[0] as? [NSDictionary] {
-          //            for pizza: NSDictionary in pizzas {
-          //              for (name, value) in pizza {
-          //                print("\(name) , \(value)")
-          //              }
-          //            }
-          //          }
-          //          for pizza in jsonResult {
-          //            if let onePizza = pizza as? [NSDictionary] {
-          //              for ingredients in onePizza {
-          //                for (key, value) in ingredients {
-          //                 print("\(key), \(value)")
-          //                }
-          //              }
-          //            }
-          //          }
         } catch { print ("Could not read the JSON file")}
       } catch { print ("Could not create the Initial Array from the JSON file")}
     }
     
-    //    if let path = Bundle.main.path(forResource: "test", ofType: "json") {
-    //      do {
-    //        let jsonData = try NSData(contentsOfFile: path, options:NSData.ReadingOptions.mappedIfSafe)
-    //        do {
-    //          let jsonResult: NSDictionary = try JSONSerialization.jsonObject(with: jsonData as Data, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-    //          if let people : [NSDictionary] = jsonResult["person"] as? [NSDictionary] {
-    //            for person: NSDictionary in people {
-    //              for (name,value) in person {
-    //                print("\(name) , \(value)")
-    //              }
-    //            }
-    //          }
-    //        } catch {}
-    //      } catch {}
-    //    }
-    
     print("hello")
-    
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    //    // get the file path for the file "test.json" in the playground bundle
-    //    let filePath = Bundle.main.path(forResource: "iOS Take Home Assignment", ofType: "json")
-    //
-    //    // get the contentData
-    //    let contentData = FileManager.default.contents(atPath: filePath!)
-    //
-    //    // get the string
-    //    let content = NSString(data: contentData!, encoding: String.Encoding.utf8.rawValue) as? String
-    //
-    //    // print
-    //    print("filepath: \(filePath!)")
-    //
-    //    if let c = content {
-    //      print("content: \n\(c)")
-    //    }
     
   }
   
@@ -181,7 +123,6 @@ class OrderViewController: UITableViewController {
     return 1
   }
   
-  
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     // #warning Incomplete implementation, return the number of rows
     
@@ -193,7 +134,6 @@ class OrderViewController: UITableViewController {
       return twenty
     }
   }
-  
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
@@ -212,52 +152,6 @@ class OrderViewController: UITableViewController {
     // Configure the cell...
     return cell
   }
-  
-  /*
-   Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-   Return false if you do not want the specified item to be editable.
-   return true
-   }
-   */
-  
-  /*
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }
-   }
-   */
-  
-  /*
-   // Override to support rearranging the table view.
-   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-   
-   }
-   */
-  
-  /*
-   // Override to support conditional rearranging of the table view.
-   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the item to be re-orderable.
-   return true
-   }
-   */
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
   
 }
 
