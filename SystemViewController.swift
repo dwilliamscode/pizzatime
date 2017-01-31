4ceea655d61daab05141ea5f6c521b9b5ae12949
dwilliamscode <DWisYourBestChoice@gmail.com>
2017-01-30 18:21:54 -0600

    removed unnecessary code

--- SystemViewController.swift
+++ SystemViewController.swift
@@ -35,14 +35,5 @@ class SystemViewController: UIViewController {
   override func viewWillDisappear(_ animated: Bool) {
     UserDefaults.standard.set(Int(topListingStepper.value), forKey: "TopOrderListCount")
   }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
